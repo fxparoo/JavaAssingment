aa6026f64c90c321cd0399f3ca3055297918de71
Akwuzie Victor <akwuziefunds@gmail.com>
2020-09-23 15:20:11 +0100

    initial

---
+++ myproject/src/com/car/Car.java
@@ -0,0 +1,28 @@
+package com.car;
+
+public class Car {
+
+    private String tyre;
+    private String headLamp;
+    private String wheelScreen;
+    private String wiper;
+
+    public void   moveForward() {
+
+    }
+
+    public void reverse() {
+
+    }
+
+    public void stop() {
+    }
+
+    public void signal() {
+    }
+
+}
+
+
+
+
---
+++ myproject/src/com/car/CarTest.java
@@ -0,0 +1,26 @@
+package com.car;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+
+      Car myCar = new Car();
+        myCar.moveForward();
+
+        Car myCar1 = new Car();
+        myCar1.reverse();
+
+        Car myCar2 = new Car();
+        myCar2.stop();
+
+        Car myCar3 = new Car();
+        myCar3.signal();
+
+    }
+
+
+
+
+
+
+}
